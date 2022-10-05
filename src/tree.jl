c3157ad143b298b8d04f63ef4b0791ce88b5d29c
lorenzoh <lorenz.ohly@gmail.com>
2022-10-05 08:40:34 +0200

    Printing improvements

--- src/tree.jl
+++ src/tree.jl
@@ -1,6 +1,6 @@
 AbstractTrees.children(invs::InvariantList) = invs.invariants
 Base.show(io::IO, inv::AbstractInvariant) = AbstractTrees.print_tree(io, inv)
 function AbstractTrees.printnode(io::IO, inv::AbstractInvariant)
-    print(io, nameof(typeof(inv)), "(\"", title(inv), "\")")
+    print(io, nameof(typeof(inv)), "(\"", md(title(inv)), "\")")
 end
 AbstractTrees.children(::AbstractInvariant) = ()
